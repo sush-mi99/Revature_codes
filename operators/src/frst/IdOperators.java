55a315b3947d04ae3a26309cf4d5c92fc768373f
susmitha vempati <susmitha.ccin008@gmail.com>
2021-06-30 09:47:16 +0530

    frst

---
+++ operators/src/frst/IdOperators.java
@@ -0,0 +1,22 @@
+package frst;
+
+public class IdOperators {
+	
+	private int x;
+	private int y;
+	public IdOperators(int x,int y) 
+	{
+		this.x=x; 
+		this.y=y;
+		
+		 while(x<=10 && y<=10){  
+			 
+		        System.out.println(x);
+		        System.out.println(y);
+		        
+		    x++;  
+		    y--;
+		    
+		    }  
+	}
+}
